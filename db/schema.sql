47a602845ef8dc1e60605cbbd7f300e7e44efbb0
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-02-07 11:28:18 -0500

    added database schema

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS burgers_DB;
+CREATE DATABASE burgers_DB;
+USE burgers_DB;
+
+CREATE TABLE burgers (
+    PRIMARY KEY id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(300) NOT NULL,
+    devoured BOOLEAN NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_DB;
+
+INSERT INTO burgers (burger_name, devoured) VALUES
+('Single with cheese', false),
+('Loaded Double Cheese Burger', false),
+('Bacon Cheddar Burger', false),
+('Jalepeno Burger', false),
+('Chili Burger', false),
+('Veggie Burger', false),
+('Breakfast Burger', false);
