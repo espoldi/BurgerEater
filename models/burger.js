14fa574ee2f97c4ff43ca768640ec84965ca6922
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-02-08 20:28:51 -0500

    added burger model

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,17 @@
 const orm = require("../config/orm.js");
 
 const burger =  {
-
+    selectAll(cb) {
+        orm.selectAll('burgers', (res) => cb(res));
+      },
+      // The variables cols and vals are arrays.
+      insertOne(cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, (res) => cb(res));
+      },
+    
+      updateOne(objColVals, condition, cb) {
+        orm.updateOne('burgers', objColVals, condition, (res) => cb(res));
+      },
 };
+
+module.exports = burger;
