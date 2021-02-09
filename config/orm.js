e963eae2ded915f6596f28064ff79c3dbf215651
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-02-08 19:19:16 -0500

    added ORM framework

---
+++ config/orm.js
@@ -0,0 +1,23 @@
+const connection = require("./configuration.js");
+
+const printQuestionMarks = (num) => {
+    const arr = [];
+    for (let i = 0; i < num; i++) {
+      arr.push('?');
+    }
+    return arr.toString();
+  };
+
+const selectAll = () => {
+
+};
+
+const insertOne = () => {
+
+};
+
+const updateOne = () => {
+
+};
+
+module.exports = orm;
