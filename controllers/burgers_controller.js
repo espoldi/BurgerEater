96bb942600e52eada2737084683a678fdb45f4ea
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-02-08 20:43:58 -0500

    added burger controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+      const hbsObject = {
+        burger: data,
+      };
+      console.log('hbsObject', hbsObject);
+      res.render('index', hbsObject);
+    });
+  });
+  
+  router.post('/api/burger', (req, res) => {
+    cat.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put('/api/burger/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    console.log('condition', condition);
+  
+    burger.update(
+      {
+        devoured: req.body.devoured,
+      },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
+
+  module.exports = router;
