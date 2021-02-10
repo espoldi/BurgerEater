ded40c27f0fbbd4f537ee085fd7c7d5d6390763a
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-02-10 00:13:22 -0500

    edited for heroku

--- config/configuration.js
+++ config/configuration.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_DB"
-});
+var connection;
+if (process.env.JAWSDB_MARIA_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_DB"
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
